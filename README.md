c744851261d31cc53a30133a75e8f7c6cffd961a
Ishant Kumawat <120731858+insanecoder02@users.noreply.github.com>
2025-02-05 12:11:10 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# A project for Week Company designed with Flutter.
+- Coding by Flutter 3.
+- Template design with Dark Mode.
+- Using sqlite in project.
+- Database backup and restore.
+- Multi-Language
+- Passcode implementation similar to the telegram program (Passcode - Fingerprint).
+- Create a PDF of the invoice.
+- Share PDF file.
+# Installation
+### Download APK 
+After clone the project, get the apk file in the APK folder and install it on your Android phone, and after installation, create fake data from the menu section.
+### [Invoice-v1.0.0.apk](./APK)
+### Installing packages
+```
+flutter pub get
+```
+### Run the project
+```
+flutter run
+```
+
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,24 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import connectivity_plus
+import local_auth_darwin
+import open_file_mac
+import path_provider_foundation
+import share_plus
+import shared_preferences_foundation
+import sqflite_darwin
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  FLALocalAuthPlugin.register(with: registry.registrar(forPlugin: "FLALocalAuthPlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+}
